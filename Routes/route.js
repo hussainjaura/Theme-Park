1e2b775714456c3c63624809b9aba0de5f97e6fc
hussainjaura <0jaurm68@solent.ac.uk>
2025-03-22 21:19:34 +0000

    made the structure of the file well and tidy

---
+++ Routes/route.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const router = express.Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Get the explore page
+router.get('/explore', (req, res) => {
+    res.sendFile(path.join(__dirname, '../structure/explore.html'));
+});
+
+// Get the rides page
+router.get('/rides', (req, res) => {
+    res.sendFile(path.join(__dirname, '../structure/rides.html'));
+});
+
+// Get the tickets page
+router.get('/tickets', (req, res) => {
+    res.sendFile(path.join(__dirname, '../structure/tickets.html'));
+});
+
+// Get the events page
+router.get('/events', (req, res) => {
+    res.sendFile(path.join(__dirname, '../structure/events.html'));
+});
+
+// Get the contact page
+router.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '../structure/contact.html'));
+});
+
+
+export default router;
+
--- src/server.js
+++ src/server.js
@@ -1,20 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import router from '../Routes/route.js';
+
+// Get the directory name equivalent in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
+
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('structure'));
+// Serve static files
+app.use('/structure', express.static(path.join(__dirname, '../structure')));
+app.use('/styling', express.static(path.join(__dirname, '../styling')));
+app.use('/images', express.static(path.join(__dirname, '../images')));
+app.use('/logo', express.static(path.join(__dirname, '../logo')));
 
-app.get('/', (req,res)=> {
-    res.sendFile(__dirname + '/structure/index.html');
+
+app.use('/', router);
+
+app.get('/', (req, res) => {
+  // Send the HTML file, using path.join() to correctly resolve the file path
+  res.sendFile(path.join(__dirname, '../structure/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
-
