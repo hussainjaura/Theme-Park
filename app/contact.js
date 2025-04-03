e9cd73fa1cadb3cd9d47089a03d522f4576a7d03
hussainjaura <0jaurm68@solent.ac.uk>
2025-04-03 09:32:32 +0100

    popup functinoality for contact page is done

--- app/contact.js
+++ app/contact.js
@@ -1,9 +1,29 @@
+function hidePopup() {
+    const popupContainer = document.getElementById('popup-container');
+    const popupMessage = document.getElementById('popup-message-text');
+    popupContainer.style.display = 'none';
+    popupMessage.textContent = '';
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const contactForm = document.getElementById('contact-form');
     const nameInput = document.getElementById('name');
     const emailInput = document.getElementById('email');
     const subjectInput = document.getElementById('subject');
     const messageInput = document.getElementById('message');
+    const popupContainer = document.getElementById('popup-container');
+    const closeButton = document.getElementById('close-button');
+    const popupMessage = document.getElementById('popup-message-text');
+
+    function showPopup(message) {
+        popupContainer.style.display = 'flex';
+        popupMessage.textContent = message;
+        closeButton.addEventListener('click', hidePopup);
+
+        setTimeout(() => {
+            hidePopup();
+        }, 10000);
+    }
 
     contactForm.addEventListener('submit', async function (e) {
         e.preventDefault();
@@ -24,13 +44,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function correctForm(formData) {
         if (!formData.name || !formData.email || !formData.subject || !formData.message) {
-            alert('Please fill in all fields');
+            showPopup('Please fill in all fields');
             return false;
         }
 
         // Basic email validation
         if (!/\S+@\S+\.\S+/.test(formData.email)) {
-            alert('Please enter a valid email');
+            showPopup('Please enter a valid email');
             return false;
         }
 
@@ -52,14 +72,16 @@ document.addEventListener('DOMContentLoaded', function () {
             console.log('Response:', data);
 
             if (response.ok) {
-                alert('Message sent successfully');
+                showPopup('Thank you for contacting us. We will get back to you as soon as possible.');
                 contactForm.reset();
             } else {
-                alert('Failed to send message: ' + data.message);
+                showPopup('Failed to send message: ' + data.message);
+                contactForm.reset();
             }
         } catch (error) {
             console.error('Error:', error);
-            alert('An error occurred while sending the message');
+            showPopup('An error occurred while sending the message. Please try again later.');
+            contactForm.reset();
         }
     }
 });
