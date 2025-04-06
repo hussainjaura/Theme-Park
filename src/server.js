c70e652e7a1b0a5524f60f466f617764864732d7
hussainjaura <0jaurm68@solent.ac.uk>
2025-04-06 14:08:14 +0100

    explore database is fixed

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
 
 // Serve static files
 app.use('/structure', express.static(path.join(__dirname, '../structure')));
